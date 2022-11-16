5ad03c2e75a4cfd355c12adb800dc08365006ea7
Aayushi ShareChat <aayushishah@sharechat.co>
2022-11-16 23:50:45 +0530

    Add Nunito font

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-
-import './App.scss';
-import { Button } from 'antd';
-import 'antd/dist/antd.css';
+import "./App.scss";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Button>Test Packeer</Button>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Button>Test Packeer</Button>
+			<h1>Hello</h1>
+		</div>
+	);
 }
 
 export default App;
